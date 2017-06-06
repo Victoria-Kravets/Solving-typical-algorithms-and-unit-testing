55fcfdc140e1880738f1c3fcb9e9f81cff5aa5a9
Victoria-Kravets <victoriakravets93@gmail.com>
2017-06-06 17:38:47 +0300

    done task 7

---
+++ Solving-typical-algorithms-and-unit-testing/ConfirmTheEnding.swift
@@ -0,0 +1,9 @@
+//
+//  ConfirmTheEnding.swift
+//  Solving-typical-algorithms-and-unit-testing
+//
+//  Created by Viktoria on 6/6/17.
+//  Copyright © 2017 Viktoria. All rights reserved.
+//
+
+import Foundation
